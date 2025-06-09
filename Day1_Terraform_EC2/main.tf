99b7f95d0b0eda51825c9cf68c562b173f0cdde7
Aryan Shukla <aryanshukla2198@gmail.com>
2025-06-09 15:21:44 +0530

    Day 2 Terraform changes

--- Day1_Terraform_EC2/main.tf
+++ Day1_Terraform_EC2/main.tf
@@ -5,4 +5,32 @@ provider "aws" {
 resource "aws_instance" "resource_name"{
     ami = var.ami_id
     instance_type = var.instance_type
+    key_name = var.key_name
+
+    tags = {
+        name = "devops-lab-instance"
+        environment = "dev"
+    }
+
+    vpc_security_group_ids = [aws_security_group.devops_sg.id]  # Attach SG
+}
+
+resource "aws_security_group" "devops_sg"{
+    name = "devops-sg"
+    description = "Allow SSH inbound traffic"
+
+    ingress{
+        description = "SSH from my IP"
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
--- Day1_Terraform_EC2/variables.tf
+++ Day1_Terraform_EC2/variables.tf
@@ -1,9 +1,15 @@
 variable "aws_region" {
-  
+  default = "ap-south-1"
 }
 
 variable "ami_id" {
-  
+  default = "ami-02a2af70a66af6dfb"
 }
 
-variable "instance_type"{}
+variable "instance_type"{
+    default = "t2.micro"
+}
+
+variable "key_name"{
+    default = "devops-key"
+}
